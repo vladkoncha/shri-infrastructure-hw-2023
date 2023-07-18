8a43031880c065d0e004f10cf3403fc30169832e
Timur Salikhov <timur.dev@mail.ru>
2023-07-19 00:48:36 +0300

    feat: test

--- src/Application.tsx
+++ src/Application.tsx
@@ -6,7 +6,7 @@ import { Home } from "./pages/Home";
 
 export const Application: FC = () => {
   const home = <Home />;
-  const about = <About />;
+  const about = <About />; 
 
   return (
     <div>
